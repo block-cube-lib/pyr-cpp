21bbea4418142f047f1b99458d509f47cd49014a
block <block@tortoise-games.com>
2017-03-15 12:27:46 +0900

    add stopwatch.hpp

---
+++ pyrite/stopwatch.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @brief     Define noncopyable.
+ *
+ * @author    block
+ *
+ * @copyright 2016 block (c)copyright all rights reserved.
+ */
+
+
+
+namespace pyrite
+{
+class stopwatch
+{
+};
+} // namespace pyrite
