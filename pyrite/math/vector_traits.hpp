bb20f92623f04e803c24c64398dba82dbd0b6376
block <block@tortoise-games.com>
2017-03-31 12:41:33 +0900

    Add comments to vector_traits.hpp

---
+++ pyrite/math/vector_traits.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * @author    block
+ * @copyright (c) 2017 block.
+ */
+
+#ifndef PYRITE_MATH_VECTOR_TRAITS_HPP
+#define PYRITE_MATH_VECTOR_TRAITS_HPP
+
+#include <pyrite/config/type.hpp>
+#include <pyrite/core/integral_constant.hpp>
+
+namespace pyrite
+{
+/**
+ * Traits for abstract access to vector type.
+ */
+template <typename T>
+struct vector_traits
+{
+  /**
+   * The type of vector.
+   */
+  using vector_type = T;
+
+  /**
+   * Element type of vector.
+   */
+  using value_type = typename T::value_type;
+
+  /**
+   * Dimension of vector.
+   */
+  static constexpr usize dimension = T::dimension;
+
+  /**
+   * Access to elements of editable vector.
+   * @param[in] vector vector
+   * @param[in] index Index of vector.
+   * @return A reference to an element of an editable vector.
+   */
+  static constexpr value_type& at( vector_type& vector, usize const& index )
+  {
+    return vector[index];
+  }
+
+  /**
+   * Access to elements of uneditable vector.
+   * @param[in] vector vector
+   * @param[in] index Index of vector.
+   * @return A reference to an element of an uneditable vector.
+   */
+  static constexpr value_type const&
+    at( vector_type const& vector, usize const& index )
+  {
+    return vector[index];
+  }
+}; // vector_traits
+} // namespace pyrite
+
+#endif // PYRITE_MATH_VECTOR_TRAITS_HPP
