a7a9f7c5a81025b25f05e30f04e00cad62fd08fe
block-cube-lib <block@tortoise-games.com>
2017-07-24 02:05:01 +0000

    fix include guard

--- pyrite/type_traits/is_comparable.hpp
+++ pyrite/type_traits/is_comparable.hpp
@@ -3,20 +3,20 @@
  * @author    block
  * @copyright (c) 2017 block.
  */
-#ifndef PYRITE_TYPE_TRAITS_IS_COMPARABLE_HPP
-#define PYRITE_TYPE_TRAITS_IS_COMPARABLE_HPP
+#ifndef PYRITE_TYPE_TRAITS_IS_EQUALITY_COMPARABLE_HPP
+#define PYRITE_TYPE_TRAITS_IS_EQUALITY_COMPARABLE_HPP
 
 #include <pyrite/core/void_t.hpp>
 
 namespace pyrite
 {
 template <typename T, typename U, typename = void>
-struct is_comparable : false_type
+struct is_equality_comparable : false_type
 {
 };
 
 template <typename T, typename U>
-struct is_comparable<
+struct is_equality_comparable<
   T,
   U,
   void_t<decltype( std::declval<T&>() == std::declval<U&>() )>> : true_type
@@ -24,13 +24,13 @@ struct is_comparable<
 };
 
 template <typename T, typename U>
-constexpr bool is_comparable_v = is_comparable<T, U>::value;
+constexpr bool is_equality_comparable_v = is_equality_comparable<T, U>::value;
 
 template <typename T>
-using is_null_comparable = is_comparable<T, std::nullptr_t>;
+using is_null_equality_comparable = is_equality_comparable<T, std::nullptr_t>;
 
 template <typename T>
-constexpr bool is_null_comparable_v = is_null_comparable<T>::value;
+constexpr bool is_null_equality_comparable_v = is_null_equality_comparable<T>::value;
 }
 
 #endif // PYRITE_TYPE_TRAITS_IS_COMPARABLE_HPP
