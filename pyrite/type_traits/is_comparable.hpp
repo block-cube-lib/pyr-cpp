5f10c38fbe912db376a6dc1439a092bfb6d2d21f
block-cube-lib <block@tortoise-games.com>
2017-07-24 01:45:28 +0000

    define is_null_comparable

--- pyrite/type_traits/is_comparable.hpp
+++ pyrite/type_traits/is_comparable.hpp
@@ -25,6 +25,12 @@ struct is_comparable<
 
 template <typename T, typename U>
 constexpr bool is_comparable_v = is_comparable<T, U>::value;
+
+template <typename T>
+using is_null_comparable = is_comparable<T, std::nullptr_t>;
+
+template <typename T>
+constexpr bool is_null_comparable_v = is_null_comparable<T>::value;
 }
 
 #endif // PYRITE_TYPE_TRAITS_IS_COMPARABLE_HPP
