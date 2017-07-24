915be039ef6952a5df2dc2bc7c223e3f124db302
block-cube-lib <block@tortoise-games.com>
2017-07-24 02:47:23 +0000

    Add comments

--- pyrite/type_traits/is_comparable.hpp
+++ pyrite/type_traits/is_comparable.hpp
@@ -10,11 +10,19 @@
 
 namespace pyrite
 {
+/**
+ * Whether T and U can be compared with operator==.
+ * @tparam Left Type.
+ * @tparam Right Type.
+ */
 template <typename T, typename U, typename = void>
 struct is_equality_comparable : false_type
 {
 };
 
+/**
+ * Template specialization when T and U can be compared with operator==.
+ */
 template <typename T, typename U>
 struct is_equality_comparable<
   T,
@@ -23,12 +31,21 @@ struct is_equality_comparable<
 {
 };
 
+/**
+ * Helper variable template of is_equality_comparable.
+ */
 template <typename T, typename U>
 constexpr bool is_equality_comparable_v = is_equality_comparable<T, U>::value;
 
+/**
+ * Whether T and nullptr can be compared with operator==.
+ */
 template <typename T>
 using is_null_equality_comparable = is_equality_comparable<T, std::nullptr_t>;
 
+/**
+ * Helper variable template of is_null_equality_comparable.
+ */
 template <typename T>
 constexpr bool is_null_equality_comparable_v = is_null_equality_comparable<T>::value;
 }
