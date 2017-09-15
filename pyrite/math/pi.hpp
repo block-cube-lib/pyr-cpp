2d535603e79c72a3fbc7722ce9a0dc3a5a2d0def
block <block@tortoise-games.com>
2017-09-15 02:17:43 +0000

    Add pi.hpp

---
+++ pyrite/math/pi.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @author    block
+ * @copyright (c) 2017 block.
+ */
+#ifndef PYRITE_MATH_PI_HPP
+#define PYRITE_MATH_PI_HPP
+
+namespace pyrite
+{
+namespace math
+{
+
+/**
+ * pi
+ * @tparam T type.
+ */
+template <typename T>
+constexpr T pi = static_cast<T>( 3.141592653589793 );
+
+} // namespace math
+} // namespace pyrite
+#endif // PYRITE_MATH_PI_HPP
