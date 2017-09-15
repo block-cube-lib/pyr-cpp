f6503c471ad78bfa7eb31678d836c1810c9bb49d
block <block@tortoise-games.com>
2017-09-15 07:48:20 +0000

    Add utility.hpp

---
+++ pyrite/math/utility.hpp
@@ -0,0 +1,62 @@
+#ifndef PYRITE_MATH_UTILITY_HPP
+#define PYRITE_MATH_UTILITY_HPP
+
+#include <type_traits>
+
+#include <pyrite/math/pi.hpp>
+
+namespace pyrite
+{
+namespace math
+{
+
+/**
+ * Convert Radian to Degree.
+ *
+ * @tparam T      Degree type.
+ * @param  radian Radian.
+ * @return Degree.
+ */
+template <typename T>
+constexpr T radian_to_degree( T const& radian ) noexcept
+{
+  return radian * T{180} / pi<T>;
+}
+
+/**
+ * Convert Degree to Radian.
+ *
+ * @tparam T      Radian type.
+ * @param  degree Degree.
+ * @return Radian.
+ */
+template <typename T>
+constexpr T degree_to_radian( T const& degree ) noexcept
+{
+  return degree / pi<T> / T{180};
+}
+
+/**
+ * Computes the absolute value.
+ *
+ * @tparam T type.
+ * @param value value.
+ * @return Absolute value.
+ */
+template <typename T>
+constexpr T abs( T value ) noexcept
+{
+  if
+    constexpr( std::is_signed<T>::value )
+    {
+      return value >= 0 ? value : -value;
+    }
+  else
+  {
+    return value;
+  }
+}
+
+} // namespace math
+} // namespace pyrite
+#endif // PYRITE_MATH_UTILITY_HPP
