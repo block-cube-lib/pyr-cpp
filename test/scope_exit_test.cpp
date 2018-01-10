0cdf0ad2681ce3075c2906944ef732eb52f9dcaf
block <block@tortoise-games.com>
2018-01-10 10:43:54 +0900

    Formatting

--- test/scope_exit_test.cpp
+++ test/scope_exit_test.cpp
@@ -22,8 +22,7 @@ TEST(scope_exit_test, function)
   value = default_value;
   EXPECT_EQ(value, default_value);
   {
-    [[maybe_unused]]
-    auto se = pyrite::make_scope_exit(&function);
+    [[maybe_unused]] auto se = pyrite::make_scope_exit(&function);
   }
   EXPECT_EQ(value, function_result);
 }
@@ -33,8 +32,7 @@ TEST(scope_exit_test, function_object)
   value = default_value;
   EXPECT_EQ(value, default_value);
   {
-    [[maybe_unused]]
-    auto se = pyrite::make_scope_exit(function_object());
+    [[maybe_unused]] auto se = pyrite::make_scope_exit(function_object());
   }
   EXPECT_EQ(value, function_object_result);
 }
@@ -44,8 +42,8 @@ TEST(scope_exit_test, lambda)
   value = default_value;
   EXPECT_EQ(value, default_value);
   {
-    [[maybe_unused]]
-    auto se = pyrite::make_scope_exit([]() { value = lambda_result; });
+    [[maybe_unused]] auto se =
+      pyrite::make_scope_exit([]() { value = lambda_result; });
   }
   EXPECT_EQ(value, lambda_result);
 }
@@ -55,8 +53,7 @@ TEST(scope_exit_test, nullptr)
   value = default_value;
   EXPECT_EQ(value, default_value);
   {
-    [[maybe_unused]]
-    pyrite::scope_exit<decltype(&function)> se(nullptr);
+    [[maybe_unused]] pyrite::scope_exit<decltype(&function)> se(nullptr);
   }
   EXPECT_EQ(value, default_value);
 }
