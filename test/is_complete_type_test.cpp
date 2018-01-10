6e49d75bfaba079838d29aa8078ff53e41d62149
block <block@tortoise-games.com>
2018-01-10 14:15:40 +0900

    is_complete_type_test is forgetten

---
+++ test/is_complete_type_test.cpp
@@ -0,0 +1,43 @@
+#include <pyrite/core/is_complete_type.hpp>
+
+#include "gtest/gtest.h"
+
+class complete_type
+{
+};
+class non_complete_type;
+class will_complete_type;
+
+TEST(is_complete_type_test, complete_type)
+{
+  using namespace pyrite;
+  EXPECT_TRUE(is_complete_type_v<complete_type>);
+  EXPECT_TRUE(is_complete_type_v<int>);
+  EXPECT_TRUE(is_complete_type_v<void*>);
+
+  class inner_function_class
+  {
+  };
+  EXPECT_TRUE(is_complete_type_v<inner_function_class>);
+}
+
+TEST(is_complete_type_test, non_complete_type)
+{
+  using namespace pyrite;
+  EXPECT_FALSE(is_complete_type_v<will_complete_type>);
+  EXPECT_FALSE(is_complete_type_v<non_complete_type>);
+  EXPECT_FALSE(is_complete_type_v<void>);
+
+  class inner_function_class;
+  EXPECT_FALSE(is_complete_type_v<inner_function_class>);
+}
+
+class will_complete_type
+{
+};
+
+TEST(is_complete_type_test, will_complete_type)
+{
+  using namespace pyrite;
+  EXPECT_FALSE(is_complete_type_v<will_complete_type>);
+}
