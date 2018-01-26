54338c46625890f47293a906a87e31b5dda92a1f
block <block@tortoise-games.com>
2018-01-26 15:33:56 +0900

    feature: Add sin

--- pyrite/math/utility.hpp
+++ pyrite/math/utility.hpp
@@ -102,5 +102,52 @@ constexpr T power(T x, std::uintmax_t y)
   return result;
 }
 
+namespace detail
+{
+constexpr std::uintmax_t end_term = 10;
+
+template <typename T>
+constexpr T sin_term_sum(T x)
+{
+  auto const term = [x](std::uintmax_t k) constexpr
+  {
+    int const t = (k == 0 || k % 2 == 0) ? 1 : -1;
+    return t * power(x, 2 * k + 1) / factorial(2 * k + 1);
+  };
+
+  T sum{0};
+  for (std::uintmax_t i = 0; i <= end_term; ++i)
+  {
+    sum += term(i);
+  }
+  return sum;
+}
+
+template <typename T>
+constexpr T sin_convert(T x)
+{
+  assert(-pi<T> <= x && x <= pi<T>);
+
+  if (abs(x) > pi<T> / 2) // x < -pi/2 || pi/2 < x
+  {
+    return sin_term_sum(mod(pi<T> - x, pi<T>) *
+                 static_cast<int>(x * 2 / pi<T>));
+  }
+  else
+  {
+    return sin_term_sum(x);
+  }
+}
+} // namespace detail
+
+template <typename T>
+constexpr auto sin(T x)
+  -> std::conditional_t<std::is_floating_point_v<T>, T, double>
+{
+  auto const sign = static_cast<int>(x / pi<T>) % 2 == 0 ? 1 : -1;
+  x               = mod(x, pi<T>); // -pi < x < pi
+  return sign * detail::sin_convert(x);
+}
+
 } // namespace pyrite::math
 #endif // PYRITE_MATH_UTILITY_HPP
