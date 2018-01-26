c0b9d751eedad2df6c964d545168c01acca60c33
block <block@tortoise-games.com>
2018-01-26 14:03:24 +0900

    feat: Add utility functions

--- pyrite/math/utility.hpp
+++ pyrite/math/utility.hpp
@@ -7,6 +7,8 @@
 #ifndef PYRITE_MATH_UTILITY_HPP
 #define PYRITE_MATH_UTILITY_HPP
 
+#include <cassert>
+#include <cstdint>
 #include <type_traits>
 
 #include <pyrite/math/constants.hpp>
@@ -58,6 +60,46 @@ constexpr T abs(T value) noexcept
   {
     return value;
   }
+}
+
+template <typename T>
+constexpr T mod(T x, T y)
+{
+  if constexpr (std::is_floating_point_v<T>)
+  {
+    return x - static_cast<std::intmax_t>(x / y) * y;
+  }
+  else
+  {
+    return x % y;
+  }
+}
+
+template <typename T = std::uintmax_t>
+T factorial(std::uintmax_t x)
+{
+  std::uintmax_t result{1};
+  for (std::uintmax_t i = 1; i <= x; ++i)
+  {
+    auto const new_value = result * i;
+    assert(result <= new_value);
+    result = new_value;
+  }
+
+  return static_cast<T>(result);
+}
+
+template <typename T>
+constexpr T power(T x, std::uintmax_t y)
+{
+  T result{1};
+
+  for (std::uintmax_t i = 1; i <= y; ++i)
+  {
+    result *= x;
+  }
+
+  return result;
 }
 
 } // namespace pyrite::math
