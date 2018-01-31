ed5b68d0494183515a28dbb7c98bb6d17594b7e5
block <block@tortoise-games.com>
2018-01-31 11:34:39 +0900

    feat: type_list all_of and any_of, none_of support for windows

--- pyrite/mpl/type_list/all_of.hpp
+++ pyrite/mpl/type_list/all_of.hpp
@@ -19,28 +19,24 @@ namespace detail
 namespace
 {
 template <typename List, template <typename> typename F>
-struct all_of_base
+struct all_of
 {
   static_assert(is_type_list_v<List>);
 };
 
 template <typename... T, template <typename> typename F>
-struct all_of_base<type_list<T...>, F>
+struct all_of<type_list<T...>, F>
 {
-private:
   static constexpr bool apply()
   {
     return sizeof...(T) == 0 || (F<T>::value && ...);
   }
-
-public:
-  using type = std::bool_constant<apply()>;
 };
 } // namespace
 } // namespace detail
 
 template <typename List, template <typename> typename F>
-struct all_of : detail::all_of_base<List, F>::type
+struct all_of : std::bool_constant<detail::all_of<List, F>::apply()>
 {
 };
 
--- pyrite/mpl/type_list/any_of.hpp
+++ pyrite/mpl/type_list/any_of.hpp
@@ -19,28 +19,24 @@ namespace detail
 namespace
 {
 template <typename List, template <typename> typename F>
-struct any_of_base
+struct any_of
 {
   static_assert(is_type_list_v<List>);
 };
 
 template <typename... T, template <typename> typename F>
-struct any_of_base<type_list<T...>, F>
+struct any_of<type_list<T...>, F>
 {
-private:
   static constexpr bool apply()
   {
     return sizeof...(T) == 0 || (F<T>::value || ...);
   }
-
-public:
-  using type = std::bool_constant<apply()>;
 };
 } // namespace
 } // namespace detail
 
 template <typename List, template <typename> typename F>
-struct any_of : detail::any_of_base<List, F>::type
+struct any_of : std::bool_constant<detail::any_of<List, F>::apply()>
 {
 };
 
