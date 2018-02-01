930827a4004b98a8df0cb5e27ea304587fbb73ec
block <block@tortoise-games.com>
2018-02-01 13:56:08 +0900

    feat: add foward decolaration of vector

---
+++ pyrite/math/vector/vector_fwd.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @author    block
+ * @copyright (c) 2018 block.
+ */
+
+#ifndef PYRITE_MATH_VECTOR_VECTOR_FWD_HPP
+#define PYRITE_MATH_VECTOR_VECTOR_FWD_HPP
+
+#include <pyrite/core/type.hpp>
+
+namespace pyrite::math
+{
+// forward decolaration
+template <typename T, usize Dimension>
+class vector;
+} // namespace pyrite::math
+
+#endif // PYRITE_MATH_VECTOR_VECTOR_FWD_HPP
