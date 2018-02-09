dc6fd84433a420434fe87bcd0d576e9e51229186
block <block@tortoise-games.com>
2018-02-09 19:13:59 +0900

    docs: add document comment to clamp

--- pyrite/utility/clamp.hpp
+++ pyrite/utility/clamp.hpp
@@ -7,12 +7,39 @@ namespace pyrite
 {
 inline namespace utility
 {
+/**
+ * if b compares less than min, return min;
+ * otherwise if max compares less than v, return max;
+ * otherwise returns v.
+ *
+ * @tparam T clamp value type.
+ * @param v   the value to clamp.
+ * @param min the boundaries to clamp to v.
+ * @param max the boundaries to clamp to v.
+ *
+ * @return reference to min if v is less than min,
+ *         reference to max if max is less than v, otherwise reference to v.
+ */
 template <typename T>
 constexpr T const& clamp(T const& v, T const& min, T const& max)
 {
   return v < min ? min : (max < v ? max : v);
 }
 
+/**
+ * if b compares less than min, return min;
+ * otherwise if max compares less than v, return max;
+ * otherwise returns v.
+ *
+ * @tparam T clamp value type.
+ * @param v    the value to clamp.
+ * @param min  the boundaries to clamp to v.
+ * @param max  the boundaries to clamp to v.
+ * @param comp comparison function object.
+ *
+ * @return reference to min if v is less than min,
+ *         reference to max if max is less than v, otherwise reference to v.
+ */
 template <typename T, typename Compare>
 constexpr T const& clamp(T const& v, T const& min, T const& max, Compare comp)
 {
